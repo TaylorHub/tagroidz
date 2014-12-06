2b8f103763e8ba9ef89cec6ddc7319e176c141c9
Morgan <morgan.guimard@gmail.com>
2014-12-06 02:30:20 +0100

    controller first commit

---
+++ tagroidz-controllers/tagroidz-cordova/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/morgan/Dev/tagroidz/tagroidz-controllers/tagroidz-cordova/platforms/android/ant-build/AndroidManifest.xml : \
+/home/morgan/Dev/tagroidz/tagroidz-controllers/tagroidz-cordova/platforms/android/AndroidManifest.xml \
---
+++ tagroidz-controllers/tagroidz-cordova/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,55 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/www/CompassError.js",
+        "id": "org.apache.cordova.device-orientation.CompassError",
+        "clobbers": [
+            "CompassError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/www/CompassHeading.js",
+        "id": "org.apache.cordova.device-orientation.CompassHeading",
+        "clobbers": [
+            "CompassHeading"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-orientation/www/compass.js",
+        "id": "org.apache.cordova.device-orientation.compass",
+        "clobbers": [
+            "navigator.compass"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/Acceleration.js",
+        "id": "org.apache.cordova.device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/accelerometer.js",
+        "id": "org.apache.cordova.device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.device-orientation": "0.3.9",
+    "org.apache.cordova.device-motion": "0.2.10",
+    "org.apache.cordova.vibration": "0.3.11"
+}
+// BOTTOM OF METADATA
+});
