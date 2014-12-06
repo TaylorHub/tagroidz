d37836411d880af871cd9644d759a043fdb16b99
Morgan <morgan.guimard@gmail.com>
2014-12-06 02:32:28 +0100

    controller first commit

--- tagroidz-controllers/tagroidz-cordova/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/morgan/Dev/tagroidz/tagroidz-controllers/tagroidz-cordova/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/morgan/Dev/tagroidz/tagroidz-controllers/tagroidz-cordova/platforms/android/CordovaLib/AndroidManifest.xml \
