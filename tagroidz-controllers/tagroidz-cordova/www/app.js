3163872ac2f55e210eead949c443f3bce9ea12d9
Morgan Guimard <morgan.guimard@docdoku.com>
2014-12-06 03:06:12 +0100

    controller

--- tagroidz-controllers/tagroidz-cordova/www/app.js
+++ tagroidz-controllers/tagroidz-cordova/www/app.js
@@ -1,5 +1,5 @@
 angular.module('tagroidz',['ngCordova'])
-.controller('Maincontroller',function($scope, $cordovaDeviceMotion,$cordovaVibration){
+.controller('Maincontroller',function($scope, $cordovaDeviceMotion,$cordovaVibration, $interval){
 
 	$scope.state = 'Hey move your phone';
 	$scope.deviceOrientation = {};
@@ -11,8 +11,18 @@ angular.module('tagroidz',['ngCordova'])
 		bottom:false
 	};
 
-	var socket =  io.connect('http://192.168.1.22:3000/controller');
+	var socket =  io.connect('http://192.168.1.37:3000/controller');
 	  
+	addEventListener('keydown',function(e){
+		$scope.dir.left = e.keyCode == 37;
+		$scope.dir.top = e.keyCode == 38;
+		$scope.dir.right = e.keyCode == 39;
+		$scope.dir.bottom = e.keyCode == 40;
+	});
+
+	$interval(function(){
+		socket.emit('state',angular.toJson($scope.dir));
+	},100)
 
     document.addEventListener('deviceready', function(){
 
@@ -26,14 +36,14 @@ angular.module('tagroidz',['ngCordova'])
 	      function(acceleration) {
 	      	angular.copy(acceleration,$scope.acceleration);
 
-	      	$scope.dir.up = acceleration.x < -Epsilon;
+	      	$scope.dir.top = acceleration.x < -Epsilon;
 	      	$scope.dir.bottom = acceleration.x > Epsilon;
 	      	$scope.dir.left = acceleration.y < -Epsilon;
 	      	$scope.dir.right = acceleration.y > Epsilon;
 
 			socket.emit('dir',$scope.dir);
 
-			if($scope.dir.up 
+			if($scope.dir.top 
 				|| $scope.dir.bottom
 				|| $scope.dir.left
 				|| $scope.dir.right
